c83967984e8904668202d0a1d7e41c6c2b1b0899
arwaahmedf <61169632+arwaahmedf@users.noreply.github.com>
2023-03-21 17:47:54 +0200

    Add files via upload

---
+++ Day1/app.js
@@ -0,0 +1,14 @@
+var task1 = require('./task1');
+ console.log(task1);
+ task1.add(15,5);
+ task1.sub(15,5);
+ task1.multi(5,5);
+ task1.multi('A',5);
+
+
+
+ var task2 = require('./task2');
+ console.log(task2);
+ task2.greet("Arwa" , new Date("1999-11-08"));
+ task2.greet("Amira" , new Date("2023-8-12"));
+ task2.greet("Ahmed" , new Date("2013-11-08"));
---
+++ Day1/task2.js
@@ -0,0 +1,16 @@
+function greet(name, birthDate) {
+    const today = new Date();
+    if(birthDate.getFullYear() < today.getFullYear())
+    {
+        const age = today.getFullYear() - birthDate.getFullYear(); 
+        console.log( `Hello ${name} and your Age Now Is : ${age}`);
+    }
+    else
+    {
+         console.log("Error");
+    }
+  }
+
+  module.exports = {
+	greet
+}
